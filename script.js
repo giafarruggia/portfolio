cb13ff6132d3842ab59c165fdbe82f12698bff38
giafarruggia <giafarruggia@gmail.com>
2026-07-22 00:15:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ const lightbox = document.getElementById("lightbox");
 const lightboxImage = document.getElementById("lightbox-image");
 const closeButton = document.querySelector(".lightbox-close");
 
-galleryImages.forEach(image => {
-    image.addEventListener("click", () => {
-        if (window.innerWidth > 700) {
-            lightbox.style.display = "flex";
-            lightboxImage.src = image.src;
-            lightboxImage.alt = image.alt;
+if (lightbox) {
+
+    galleryImages.forEach(image => {
+        image.addEventListener("click", () => {
+            if (window.innerWidth > 700) {
+                lightbox.style.display = "flex";
+                lightboxImage.src = image.src;
+                lightboxImage.alt = image.alt;
+            }
+        });
+    });
+
+    closeButton.addEventListener("click", () => {
+        lightbox.style.display = "none";
+    });
+
+    lightbox.addEventListener("click", (event) => {
+        if (event.target === lightbox) {
+            lightbox.style.display = "none";
         }
     });
-});
 
-closeButton.addEventListener("click", () => {
-    lightbox.style.display = "none";
-});
-
-lightbox.addEventListener("click", (event) => {
-    if (event.target === lightbox) {
-        lightbox.style.display = "none";
-    }
-});
+}
